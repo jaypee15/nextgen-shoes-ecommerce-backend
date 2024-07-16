3a94fa05861313954c1a48dac0078c73c63a0651
emekaobioji23 <emekaobioji23@gmail.com>
2024-07-16 15:02:28 +0100

    add uncommented saves for order, cart

--- controllers/checkout-controller.js
+++ controllers/checkout-controller.js
@@ -176,10 +176,10 @@ exports.processCart = catchAsync(async (req, res) => {
         };
         if (validateOrder(orderProperties, res)) {
             const order = new Order(orderProperties);
-            //await order.save();
-            // Clear the cart
-            //cart.items = [];
-            //await cart.save();
+            await order.save();
+            //Clear the cart
+            cart.items = [];
+            await cart.save();
             req.body.amount = totalAmount;
             req.body.orderId=order.id;
             req.body.redirect_url = `${req.protocol}://${req.get("host")}${PAYMENT_CALLBACK_URL}/${userId}?` +
